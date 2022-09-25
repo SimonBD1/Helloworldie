f2efc18524f5487816ac7e257825e22c633ecfb0
Tobias <tobi4533@stud.kea.dk>
2022-09-25 13:24:57 +0200

    Added facts 3.0

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("SUUUUIIII, Kjartan lugter!!");
         System.out.println("Simon har store muskler");
         System.out.println("Kamelåså");
+        System.out.println("KJartan er dogwater");
     }
 }
