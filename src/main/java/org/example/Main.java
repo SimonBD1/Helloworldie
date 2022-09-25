13c655ab02f3f68d6081afd4575a2fbe1fb2a850
SimonBD1 <simo20k3@stud.kea.dk>
2022-09-25 13:20:10 +0200

    suii?

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! Suuuii!");
         System.out.println("SUUUUIIII, Kjartan lugter!!");
+        System.out.println("Simon har store muskler");
     }
 }
