d16246b703d88506302f3be82780c3768040b5cf
Tobias <tobi4533@stud.kea.dk>
2022-09-25 13:16:50 +0200

    Added facts 2.0

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Suuuii!");
+        System.out.println("Shit, Kjartan er langsom! Suuuii!");
         System.out.println("SUUUUIIII, Kjartan lugter!!");
     }
 }
