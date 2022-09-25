6fea39d784e2c84d9dfa3bc10c2082efb8083aff
Tobias <tobi4533@stud.kea.dk>
2022-09-25 13:18:35 +0200

    Added facts 3.0

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Shit, Kjartan er langsom! Suuuii!!");
+        System.out.println("Shit, Kjartan er langsom og barnlig! Suuuii!!");
         System.out.println("SUUUUIIII, Kjartan lugter!!");
     }
 }
