83a8bdafd60253a1b7abdd19ba9199b15cb56880
kjartanLeander <Kjar0321@stud.kea.dk>
2022-09-25 13:37:56 +0200

    Tobias

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("Kamelåså");
         System.out.println("KJartan er dogwater!");
         System.out.println("Peter har en kæmpe johnson");
+        System.out.println("Tobias er forelsket i Carl");
     }
 }
